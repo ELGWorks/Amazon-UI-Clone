598991d6728e57a87396480f17b9f63b6c52ab9d
ELGWorks <244444991+ELGWorks@users.noreply.github.com>
2026-08-30 21:16:00 +0800

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,8 +4,48 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/backend-practice.js';
 import '../data/car.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve('value1');
+//     });
+
+// }).then((value) => {
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//         console.log(value);
+//     });
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
+
+// loadProducts(() => {
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
